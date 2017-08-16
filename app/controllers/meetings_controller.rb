cc76b24f1aeb7f3398d1602b890fb60f4ed7abef
asder707 <abn.sebick@gmail.com>
2017-08-15 23:20:13 -0300

    Creating, show and list meetings

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,4 +1,24 @@
 class MeetingsController < ApplicationController
+    def index
+        @meetings = Meeting.all
+    end
+   
+    def show
+        @meeting = Meeting.find(params[:id])
+    end
+
     def new
     end
+
+    def create
+        @meeting = Meeting.new(article_parmams)
+
+        @meeting.save
+        redirect_to @meeting
+    end
+
+    private
+        def article_parmams
+            params.require(:meeting).permit(:title, :description)
+        end
 end
