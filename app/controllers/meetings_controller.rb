662b4111371d1bb35aa55c6875ecb5558243ec9a
asder707 <abn.sebick@gmail.com>
2017-08-21 20:48:12 -0300

    Working the tutorial

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -8,17 +8,42 @@ class MeetingsController < ApplicationController
     end
 
     def new
+        @meeting = Meeting.new
+    end
+
+    def edit
+        @meeting = Meeting.find params[:id]
     end
 
     def create
-        @meeting = Meeting.new(article_parmams)
+        @meeting = Meeting.new(meeting_params)
 
-        @meeting.save
-        redirect_to @meeting
+        if @meeting.save
+            redirect_to @meeting
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @meeting = Meeting.find(params[:id])
+
+        if @meeting.update meeting_params
+            redirect_to @meeting
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @meeting = Meeting.find(params[:id])
+        @meeting.destroy
+
+        redirect_to meetings_path
     end
 
     private
-        def article_parmams
+        def meeting_params
             params.require(:meeting).permit(:title, :description)
         end
 end
